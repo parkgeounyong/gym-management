8f7353ed2c136c30d236559d60948f6e992b3f38
gyparkCap <gy.park.cap@gmail.com>
2025-05-01 15:45:48 +0900

    feature-1 db 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.postgresql:postgresql:42.7.5")
 }
 
 kotlin {
