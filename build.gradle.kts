be977480489c7127b94badff61abbc43a6e9ce75
gyparkCap <gy.park.cap@gmail.com>
2025-05-04 16:53:58 +0900

    feature-1 기본 시큐리티 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.postgresql:postgresql:42.7.5")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
-
+	implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 kotlin {
---
+++ src/main/kotlin/com/gym.management/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.gym.management.config
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@SecurityScheme(type = SecuritySchemeType.HTTP, name = "bearerAuth", scheme = "bearer")
+class SecurityConfig() {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers(
+                        "/swagger-ui/**", "/v3/api-docs/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+            }
+        return http.build()
+    }
+}
