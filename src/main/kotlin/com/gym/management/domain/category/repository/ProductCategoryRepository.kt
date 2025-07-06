b4ea51fa96637c8ddf23a89813f3a66ffcd9cf6c
gyparkCap <gy.park.cap@gmail.com>
2025-07-06 17:34:10 +0900

    feature-3 ProductCategory 테이블 생성 및 엔티티 생성

---
+++ src/main/kotlin/com/gym/management/domain/category/repository/ProductCategoryRepository.kt
@@ -0,0 +1,10 @@
+package com.gym.management.domain.category.repository
+
+import com.gym.management.domain.category.model.entity.ProductCategory
+import com.gym.management.domain.category.model.entity.id.ProductCategoryId
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductCategoryRepository : JpaRepository<ProductCategory, ProductCategoryId> {
+}
