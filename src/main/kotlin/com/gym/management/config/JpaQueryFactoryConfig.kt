8edc04da782573772e4478d361aff2c0bb893777
gyparkCap <gy.park.cap@gmail.com>
2025-08-16 20:00:03 +0900

    JpaQueryFactoryConfig lateInit 제거

--- src/main/kotlin/com/gym/management/config/JpaQueryFactoryConfig.kt
+++ src/main/kotlin/com/gym/management/config/JpaQueryFactoryConfig.kt
@@ -2,15 +2,11 @@ package com.gym.management.config
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.persistence.EntityManager
-import jakarta.persistence.PersistenceContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class JpaQueryFactoryConfig {
-    @PersistenceContext
-    private lateinit var entityManager: EntityManager
-
+class JpaQueryFactoryConfig(private val entityManager: EntityManager) {
     @Bean
     fun jpaQueryFactory() = JPAQueryFactory(entityManager)
 }
