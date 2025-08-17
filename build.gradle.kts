92fa2e472fb9ea9cc48b6e29ac3377a62e93abbf
gyparkCap <gy.park.cap@gmail.com>
2025-08-17 15:56:40 +0900

    sagger 버전 문제 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.postgresql:postgresql:42.7.5")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.0")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("io.github.microutils:kotlin-logging:3.0.5")
 	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
