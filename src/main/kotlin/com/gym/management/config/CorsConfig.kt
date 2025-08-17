c98607ad418a57abb336715fa2313699f9788eca
gyparkCap <gy.park.cap@gmail.com>
2025-08-17 16:11:57 +0900

    환경별 cors 설정

--- src/main/kotlin/com/gym/management/config/CorsConfig.kt
+++ src/main/kotlin/com/gym/management/config/CorsConfig.kt
@@ -1,5 +1,6 @@
 package com.gym.management.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -7,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Profile("local")
 @Configuration
-class CorsConfig : WebMvcConfigurer {
+class CorsConfig(
+    @Value("\${cors.allowed-origins}") protected val allowedOrigins: String
+) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("*")
+            .allowedOriginPatterns(allowedOrigins)
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true)
     }
