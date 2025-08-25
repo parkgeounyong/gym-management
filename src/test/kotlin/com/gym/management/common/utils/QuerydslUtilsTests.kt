13b94c06c7a17b2d344abc0acdb1a28f34e32641
gyparkCap <gy.park.cap@gmail.com>
2025-08-26 06:43:33 +0900

    QuerydslUtilsTests.buildEqualsIfPresent 작성

--- src/test/kotlin/com/gym/management/common/utils/QuerydslUtilsTests.kt
+++ src/test/kotlin/com/gym/management/common/utils/QuerydslUtilsTests.kt
@@ -7,8 +7,8 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 
 class QuerydslUtilsTests : BehaviorSpec({
+    val user = QUser.user
     given("createSort(entity: EntityPathBase<*>, sortBy: String, direction: String) 테스트") {
-        val user = QUser.user
         `when`("direction이 ASC일 때") {
             val sort = QuerydslUtils.createSort(user, "name", "ASC")
             then("정렬 순서는 ASC여야 한다") {
@@ -31,6 +31,21 @@ class QuerydslUtilsTests : BehaviorSpec({
             val sort = QuerydslUtils.createSort(user, "userCreatedAt", "ASC")
             then("정렬 대상이 userCreatedAt이여야 한다") {
                 sort.target.toString() shouldContain "userCreatedAt"
+            }
+        }
+    }
+
+    given("buildEqualsIfPresent(path: SimpleExpression<T>, value: T?)"){
+        `when`("value가 null일 때") {
+            val builder = QuerydslUtils.buildEqualsIfPresent(user.userName, null)
+            then("조건이 없어야 한다") {
+                builder.value shouldBe null
+            }
+        }
+        `when`("value가 존재할 때") {
+            val builder = QuerydslUtils.buildEqualsIfPresent(user.userName, "test")
+            then("조건이 있어야 한다") {
+                builder.value shouldBe user.userName.eq("test")
             }
         }
     }
