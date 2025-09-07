dc053a09d6865790683f106ab2078ad04d5deeb9
parkgeounyong <50165260+parkgeounyong@users.noreply.github.com>
2025-09-07 17:24:54 +0900

    feature-48 product에 대하여 bulk insert 구현 (#51)
    Co-authored-by: gyparkCap <gy.park.cap@gmail.com>

--- src/main/kotlin/com/gym/management/domain/product/service/DProductService.kt
+++ src/main/kotlin/com/gym/management/domain/product/service/DProductService.kt
@@ -3,14 +3,10 @@ package com.gym.management.domain.product.service
 import com.gym.management.domain.product.model.ProductDTOMapper.toProductDto
 import com.gym.management.domain.product.model.ProductMapper.toDto
 import com.gym.management.domain.product.model.ProductMapper.toEntity
-import com.gym.management.domain.product.model.ProductMapper.update
 import com.gym.management.domain.product.repository.ProductRepository
 import com.gym.management.domain.product.model.dto.ProductDTO
-import com.gym.management.domain.product.model.entity.Product
-import com.gym.management.domain.product.model.entity.id.ProductId
 import com.gym.management.domain.product.model.request.CreateProductRequest
 import com.gym.management.domain.product.model.request.UpdateProductRequest
-import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -18,7 +14,6 @@ import java.time.LocalDateTime
 @Service
 class DProductService(
     private val productRepository: ProductRepository,
-    private val entityManager: EntityManager,
 ) {
     fun createProduct(createProductRequest: CreateProductRequest): ProductDTO {
         return productRepository.save(createProductRequest.toProductDto().toEntity())
@@ -26,31 +21,8 @@ class DProductService(
     }
 
     @Transactional
-    fun upsertProduct(updateProductRequests: List<UpdateProductRequest>): Boolean {
-        val idList = updateProductRequests.map { ProductId(it.branchId, it.productCode) }
-        val existingEntities =
-            productRepository.findAllById(idList).associateBy { ProductId(it.branchId, it.productCode) }
-
-        val toInsert = mutableListOf<Product>()
-
-        updateProductRequests.map { it.toProductDto() }
-            .forEach { dto ->
-                val key = ProductId(dto.branchId, dto.productCode)
-                val entity = existingEntities[key]
-                if (entity != null) {
-                    entity.update(dto)
-                } else {
-                    toInsert.add(dto.toEntity())
-                }
-            }
-
-        toInsert.forEachIndexed { index, entity ->
-            entityManager.persist(entity)
-            if (index % 100 == 0) {
-                entityManager.flush()
-                entityManager.clear()
-            }
-        }
+    fun upsertProduct(updateProductRequestList: List<UpdateProductRequest>): Boolean {
+        productRepository.bulkUpsertProduct(updateProductRequestList.map { it.toProductDto().toEntity() })
         return true
     }
 
